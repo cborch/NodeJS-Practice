f76207759e6152ba8c1e1e33835cb7e5795d7622
Carter Borchetta <carterborch@me.com>
2021-08-09 18:34:33 -0400

    Added persistence via nedb database

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const express = require('express')
 // Express basically comes in as a big func that we can then run
 const app = express()
 
+const Datastore = require('nedb')
+
 // A major thign we need to do is have the app start listening
 // We give it the port we want and then the func to do when its started listening
 app.listen(3000, () => console.log('listening at 3000'))
@@ -38,16 +40,32 @@ app.use(express.json({ limit: '1mb' }))
 // - html is then sent to the client where it is rendered
 // - If the html contains js, it is sent as text and then executed at the client 
 
+const database = new Datastore('database.db')
+database.loadDatabase()
+
 // Setting up Routing
 
 // Specifying a POST route
 // request contains the info that the client is sneding up
 // response allows us to send stuff back to the user
 app.post('/api', (request, response) => {
-    console.log(request.body)
+    //console.log(request.body)
+    const data = request.body
+    const timestamp = Date.now()
+    data.timestamp = timestamp
+    database.insert(data)
     response.json({
         status: "success",
         latitide: request.body.lat,
-        longitude: request.body.long
+        longitude: request.body.long,
+        color: request.body.color,
+        timestamp: timestamp
     })
 })
+
+
+// Now we need to add a DB for persistence
+// - MongoDB is popular
+//  - MOngoDB is a document based DB which means files are stored in files similar to JSON documents
+// We'll use NE-DB
+// - Its a subset of MongoDB's API
